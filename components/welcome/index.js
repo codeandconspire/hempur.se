9b4d2569f8ef2b8efd17f36ce59ec5a63b247c25
Anton Trollbäck <anton@trollback.se>
2018-10-11 17:10:18 +0200

    Refactor text in welcome component

--- components/welcome/index.js
+++ components/welcome/index.js
@@ -13,10 +13,12 @@ function welcome (props) {
     <div class="Welcome">
       <div class="u-container">
         <img aria-hidden="true" role="presentational" class="Welcome-background" width="1018" height="1244" sizes="(min-width: 1000px) 563px, 266px" srcset="${srcset(background, [300, 500, 900, [1200, 'q_50']], { type: 'upload' })}" src="/media/upload/q_auto,w_266/${background}" alt="${text`Picture of Hempur toilet roll`}">
-        <h1 class="Welcome-heading">
-          Precis som vanligt, fast helt annorlunda<br>
-          <small class="Welcome-subheading">Toapapper av bambu, alltså.</small>
-        </h1>
+        <div class="Welcome-body">
+          <div class="Text">
+            <h1 class="Text-stack">Precis som vanligt, fast helt annorlunda</h1>
+            <p><strong>Toapapper av bambu, alltså.</strong></p>
+          </div>
+        </div>
         <img class="Welcome-image" width="318" height="256" sizes="(min-width: 1000px) 644px, 100vw" srcset="${srcset(image, [400, 600, 900, [1200, 'q_50']], { type: 'upload' })}" src="/media/upload/q_auto,w_644/${image}" alt="${text`Picture of Hempur toilet paper 6-pack`}">
         ${button({ href: '/toa-6-pack', text: 'Produktinfo' })}
       </div>
