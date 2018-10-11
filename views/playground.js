27dd70a99b5ce34f42f292c733a4a5eb85be7215
Anton Trollbäck <anton@trollback.se>
2018-10-11 16:55:52 +0200

    Initial work on text alignments

--- views/playground.js
+++ views/playground.js
@@ -6,30 +6,15 @@ function playground (state, emit) {
   return html`
     <main class="View-main">
       <article class="u-container">
-        <div class="Text">
-          <h1>Playground</h1>
+        <div class="Text Text--center">
+          <h5>Playground</h5>
+          <h1 class="Text-h2">Where is all happens </h1>
           <p>This is the playground page where we can try stuff out. Like <a href="http://blip.nu/">perhaps an inline link</a>, or <strong>strong text</strong>.</p>
           <p>Seems to work just fine ✅ right.</p>
-          <p class="Text-large">Large paragraph! Should blend in above 1000px viewport width.</p>
-          <h1>Title 1 with a <br />line break</h1>
-          <p>Regular paragraph.</p>
-          <h2>Title 2 with a <br />line break</h2>
-          <p>Regular paragraph.</p>
-          <h3>Title 3 with a <br />line break</h3>
-          <p>Regular paragraph.</p>
-          <h4>Title 4 with a <br />line break</h4>
-          <p>Regular paragraph.</p>
-          <h5>Title 5, also known as "label", with a <br />line break</h5>
-          <p>Regular paragraph.</p>
-          <ul>
-            <li>Unordered list item</li>
-            <li>Unordered list item</li>
-          </ul>
-          <p>Regular paragraph.</p>
-          <ol>
-            <li>Ordered list item</li>
-            <li>Ordered list item</li>
-          </ol>
+          <p><br/>Kontakta oss här</p>
+          <h4>hej@hempur.se <br>+46 701 234 567</h4>
+          <h3>Vanliga frågor</h3>
+          <h3>Om bambu</h3>
         </div>
       </article>
     </main>
