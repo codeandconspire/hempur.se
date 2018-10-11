96dc4968979a2cc6dfed22ec0d6090344e70d8dd
Carl Törnqvist <carl@tornqv.ist>
2018-10-11 15:32:17 +0200

    Add stack, banner slices

--- components/welcome/index.js
+++ components/welcome/index.js
@@ -1,6 +1,8 @@
 var html = require('choo/html')
 var button = require('../button')
-var { srcset } = require('../base')
+var { srcset, i18n } = require('../base')
+
+var text = i18n()
 
 module.exports = welcome
 
@@ -10,12 +12,12 @@ function welcome (props) {
   return html`
     <div class="Welcome">
       <div class="u-container">
-        <img aria-hidden="true" role="presentational" class="Welcome-background" width="1018" height="1244" sizes="(min-width: 1000px) 563px, 266px" srcset="${srcset(background, [300, 500, 900, [1200, 'q_50']], { type: 'upload' })}" src="/media/upload/q_auto,w_266/${background}">
+        <img aria-hidden="true" role="presentational" class="Welcome-background" width="1018" height="1244" sizes="(min-width: 1000px) 563px, 266px" srcset="${srcset(background, [300, 500, 900, [1200, 'q_50']], { type: 'upload' })}" src="/media/upload/q_auto,w_266/${background}" alt="${text`Picture of Hempur toilet roll`}">
         <h1 class="Welcome-heading">
           Precis som vanligt, fast helt annorlunda<br>
           <small class="Welcome-subheading">Toapapper av bambu, alltså.</small>
         </h1>
-        <img class="Welcome-image" width="318" height="256" sizes="(min-width: 1000px) 644px, 100vw" srcset="${srcset(image, [400, 600, 900, [1200, 'q_50']], { type: 'upload' })}" src="/media/upload/q_auto,w_644/${image}">
+        <img class="Welcome-image" width="318" height="256" sizes="(min-width: 1000px) 644px, 100vw" srcset="${srcset(image, [400, 600, 900, [1200, 'q_50']], { type: 'upload' })}" src="/media/upload/q_auto,w_644/${image}" alt="${text`Picture of Hempur toilet paper 6-pack`}">
         ${button({ href: '/toa-6-pack', text: 'Produktinfo' })}
       </div>
     </div>
--- views/home.js
+++ views/home.js
@@ -1,5 +1,6 @@
 var html = require('choo/html')
 var welcome = require('../components/welcome')
+var button = require('../components/button')
 
 module.exports = home
 
@@ -7,6 +8,38 @@ function home (state, emit) {
   return html`
     <main class="View-main View-main--stack">
       ${welcome()}
+      <!-- slice -->
+      <div class="Text u-textCenter u-spaceT6">
+        <div class="u-spaceV4 u-spaceH4">
+          <img width="135" height="76" src="https://via.placeholder.com/135x76" alt="">
+          <h2 class="Text-h4 u-spaceT0">Universums mjukaste</h2>
+          <p>Visste du att bambuträd kan växa upp till en meter om dagen och är en av världens mest förnybara växter?</p>
+        </div>
+        <div class="u-spaceV4 u-spaceH4">
+          <img width="135" height="76" src="https://via.placeholder.com/135x76" alt="">
+          <h2 class="Text-h4 u-spaceT0">Universums mjukaste</h2>
+          <p>Visste du att bambuträd kan växa upp till en meter om dagen och är en av världens mest förnybara växter?</p>
+        </div>
+        <div class="u-spaceV4 u-spaceH4">
+          <img width="135" height="76" src="https://via.placeholder.com/135x76" alt="">
+          <h2 class="Text-h4 u-spaceT0">Universums mjukaste</h2>
+          <p>Visste du att bambuträd kan växa upp till en meter om dagen och är en av världens mest förnybara växter?</p>
+        </div>
+      </div>
+      <div class="u-textCenter u-spaceV6">
+        ${button({ class: 'Button--invert', href: '/toa-6-pack', text: 'All nördig info' })}
+      </div>
+      <!-- /slice -->
+      <!-- slice -->
+      <div class="u-nbfc u-textCenter u-bgCurrent u-bgWhite">
+        <img class="u-inlineBlock u-spaceV3 u-spaceH2" width="45" height="45" src="https://via.placeholder.com/45x45" alt="">
+        <img class="u-inlineBlock u-spaceV3 u-spaceH2" width="45" height="45" src="https://via.placeholder.com/45x45" alt="">
+        <img class="u-inlineBlock u-spaceV3 u-spaceH2" width="45" height="45" src="https://via.placeholder.com/45x45" alt="">
+        <img class="u-inlineBlock u-spaceV3 u-spaceH2" width="45" height="45" src="https://via.placeholder.com/45x45" alt="">
+      </div>
+      <!-- /slice -->
+
+      <!-- slice -->
     </main>
   `
 }
