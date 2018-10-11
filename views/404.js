6137df39d8b45e0ea88881e323dbb252079d8948
Carl Törnqvist <carl@tornqv.ist>
2018-10-11 12:23:23 +0200

    Fix 404 status

--- views/404.js
+++ views/404.js
@@ -2,8 +2,11 @@ var view = require('../components/view')
 
 module.exports = view(notFound)
 
-function notFound () {
+function notFound (state, emit) {
   var err = new Error('Page not found')
   err.status = 404
+  if (state.prefetch) {
+    state.prefetch.push(Promise.reject(err))
+  }
   throw err
 }
