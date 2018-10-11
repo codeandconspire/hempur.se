f087e951aea9f948d264f4e4a3502e91d825c2ac
Anton Trollbäck <anton@trollback.se>
2018-10-11 14:33:59 +0200

    Add some dummy content

--- views/playground.js
+++ views/playground.js
@@ -5,7 +5,13 @@ module.exports = playground
 function playground (state, emit) {
   return html`
     <main class="View-main">
-      playground
+      <article class="u-container">
+        <div class="Text">
+          <h1>Playground</h1>
+          <p>This is the playground page where we can try stuff out. Like <a href="http://blip.nu/">perhaps an inline link</a>, or <strong>strong text</strong>.</p>
+          <p>Seems to work just fine ✅ right.</p>
+        </div>
+      </article>
     </main>
   `
 }
