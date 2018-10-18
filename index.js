fe91e3bb5588c35ff10d3a2fef613071a51aa628
Carl Törnqvist <carl@tornqv.ist>
2018-10-18 10:08:31 +0200

    Prefetch docs on view load

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var choo = require('choo')
+var { Predicates } = require('prismic-javascript')
 var View = require('./components/view')
 
 var REPOSITORY = 'https://hempur.cdn.prismic.io/api/v2'
@@ -18,9 +19,18 @@ app.use(require('./stores/prismic')({
 }))
 app.use(require('./stores/meta'))
 
-app.route('/', View.create(() => import('./views/home')))
-app.route('/produkter/:uid', View.create(() => import('./views/product')))
-app.route('/:uid', View.create(() => import('./views/page')))
+app.route('/', View.create(
+  () => import('./views/home'),
+  prefetch((state) => Predicates.at('document.type', 'home'))
+))
+app.route('/produkter/:uid', View.create(
+  () => import('./views/product'),
+  prefetch((state) => Predicates.at('my.product.uid', state.params.uid))
+))
+app.route('/:uid', View.create(
+  () => import('./views/page'),
+  prefetch((state) => Predicates.at('my.page.uid', state.params.uid))
+))
 
 if (process.env.NODE_ENV === 'development') {
   app.route('/playground', View.create(() => import('./views/playground')))
@@ -32,5 +42,14 @@ try {
   if (typeof window !== 'undefined') {
     document.documentElement.removeAttribute('scripting-enabled')
     document.documentElement.setAttribute('scripting-initial-only', '')
+  }
+}
+
+// prefetch document while view is loading
+// fn -> fn
+function prefetch (predicate) {
+  return function (state, emit) {
+    state.prismic.get(predicate(state), { prefetch: true })
+    return document.body
   }
 }
